78e3076945a3f924b74ca709940337dc3ddad6df
TalhaSheikh-dev <talhakhawar9292@gmail.com>
2020-11-09 12:39:17 +0500

    Add files via upload

---
+++ feature_extraction.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 11:42:58 2020
+
+@author: talha
+"""
+
+
+from numpy import load
+from numpy import asarray
+from numpy import expand_dims
+from numpy import savez_compressed
+from numpy import reshape
+from keras.models import load_model 
+
+#Generalize the data and extract the embeddings
+def extract_embeddings(model,face_pixels):
+  face_pixels = face_pixels.astype('float32')  #convert the entire data to float32(base)
+  mean = face_pixels.mean()                    #evaluate the mean of the data
+  std  = face_pixels.std()                     #evaluate the standard deviation of the data
+  face_pixels = (face_pixels - mean)/std       
+  samples = expand_dims(face_pixels,axis=0)    #expand the dimension of data 
+  yhat = model.predict(samples)
+  return yhat[0]
+
+#load the compressed dataset and facenet keras model
+data = load('lfw-dataset.npz')
+trainx, trainy = data['arr_0'],data['arr_1']
+print(trainx.shape, trainy.shape)
+model = load_model('facenet_keras.h5')
+
+#get the face embeddings
+new_trainx = list()
+for train_pixels in trainx:
+  embeddings = extract_embeddings(model,train_pixels)
+  new_trainx.append(embeddings)
+new_trainx = asarray(new_trainx)             #convert the embeddings into numpy array
+print(new_trainx.shape)
+
+#compress the 128 embeddings of each face 
+savez_compressed('lfw-embeddings.npz',new_trainx,trainy)
